fba0b092d4f4ee878c497e2c4061077af3112135
sorleuCode <muhammedsoliu2222@gmail.com>
2025-02-02 10:36:40 +0100

    upadte

--- frontend/src/pages/BorrowPage.jsx
+++ frontend/src/pages/BorrowPage.jsx
@@ -407,6 +407,7 @@ const BorrowPage = () => {
                     <span>
                       {formData.amount || 0} {formData.selectedBorrowToken}
                     </span>
+                    
                   </div>
                   <div className="flex justify-between text-sm text-gray-400">
                     <span>Duration</span>
